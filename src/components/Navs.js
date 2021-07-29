4b6ccb9d1723431fe982d6fa09148aeedfa6810b
pankhil <rithik.marwaha6@gmail.com>
2021-07-29 10:58:02 +0530

    added search results

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Navs = () => {
-    const LINKS=[
-        {to: '/',text:'Home'},
-        {to: '/starred',text:'Starred'}
-    ]
+const LINKS = [
+    { to: '/', text: 'Home' },
+    { to: '/starred', text: 'Starred' },
+  ];
+  
+  const Navs = () => {
     return (
-        <div>
-           <ul>
-                {LINKS.map(item=>(
-                    <li key={item.to}>
-                        <Link to={item.to}>{item.text}
-                        </Link>
-                    </li>
-                ))
-                }
-            </ul> 
-        </div>
-    )
-}
-
-export default Navs
+      <div>
+        <ul>
+          {LINKS.map(item => (
+            <li key={item.to}>
+              <Link to={item.to}>{item.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Navs;
+  
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
 import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import { apiGet } from '../misc/config'
 
 const Home = () => {
     
     const [input,setInput]=useState('')
+    const [results,setResults]=useState(null)
     
     const onSearch=()=>{
-        fetch(` https://api.tvmaze.com/singlesearch/shows?q=${input}`)
-        .then(r=> r.json())
-        .then(result=>console.log(result))
+        apiGet(`/search/shows?q=${input}`).then(result => {
+            setResults(result);
+          });
     }
 
     const onInputChange= ev=>{
@@ -21,10 +23,25 @@ const Home = () => {
         onSearch()
     }
 
+    const renderResults=()=>{
+        if(results && results.length===0){
+            return <div>No results</div>
+        }
+        if (results && results.length>0){
+            return (<div>
+                {results.map(item=>(
+                    <div key='item.show.id'>{item.show.name}</div>
+                ))}
+            </div>)
+        }
+        return null
+    }
+
     return (
         <MainPageLayout>
             <input type='text' onChange={onInputChange} onKeyDown={onKeyPressed} value={input}/>
             <button type='button' onClick={onSearch}>Search</button> 
+            {renderResults()}
         </MainPageLayout>
     )
 }

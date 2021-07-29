98d6b4264df00859bca5510242ff71af138499e4
pankhil <rithik.marwaha6@gmail.com>
2021-07-29 12:19:29 +0530

    added preview cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React,{useState} from 'react'
+import ActorGrid from '../actors/ActorGrid'
 import MainPageLayout from '../components/MainPageLayout'
 import { apiGet } from '../misc/config'
+import ShowGrid from '../shows/ShowGrid'
 
 const Home = () => {
     
@@ -33,11 +35,7 @@ const Home = () => {
             return <div>No results</div>
         }
         if (results && results.length > 0) {
-            return results[0].show
-              ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-              : results.map(item => (
-                  <div key={item.person.id}>{item.person.name}</div>
-                ));
+            return results[0].show ?<ShowGrid data={results} /> : <ActorGrid data={results} />
         }
         return null
     }

72aa4aba0041b296ae5ac9e3610d04f1b1e58b70
pankhil <rithik.marwaha6@gmail.com>
2021-08-06 12:40:44 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Route,Switch} from 'react-router-dom'
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
      
     <Switch>
         
@@ -27,7 +35,7 @@ function App() {
         </Route>
 
     </Switch>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { TitleWrapper } from './Title.Styled';
 
-const Title = ({title,subtitle}) => {
+const Title = ({ title, subtitle }) => {
     return (
-        <div>
-            <h1>{title}</h1>
-            <p>{subtitle}</p>
-        </div>
-    )
-}
-
-export default Title
+      <TitleWrapper>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </TitleWrapper>
+    );
+  };
+  
+  export default Title;
+  
